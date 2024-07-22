c559d222824fe4f685d8260ce0de67e36849d792
iamdaviddev <davidpaulodev@gmail.com>
2024-07-22 22:01:12 +0100

    first db table

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,26 @@
-console.log("Hello world!");
+import fastify from "fastify";
+import { prisma } from "../lib/prisma";
+
+const app = fastify();
+
+app.get("/cadastrar", async () => {
+  await prisma.trip.create({
+    data: {
+      destination: 'Accra',
+      starts_at: new Date(),
+      ends_at: new Date(),
+    }
+  })
+
+  return 'Registro cadastrado com sucesso';
+});
+
+app.get("/listar", async () => {
+  const trips = await prisma.trip.findMany();
+
+  return trips;
+});
+
+app.listen({ port: 3333 }).then(() => {
+  console.log("Server is running on port 3333");
+})
